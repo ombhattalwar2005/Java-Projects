d6626dd301a6d3a364cec596155028cd7122bc9d
Om Bhattalwar <164125899+ombhattalwar2005@users.noreply.github.com>
2025-04-23 00:27:01 +0530

    Update Sorters2120.java

--- Sorters2120.java
+++ Sorters2120.java
@@ -53,7 +53,7 @@ public class Sorters2120 {
                 comparedIndex++;
                 selectionSort(theList);
             }
-            else if(minElement.compareTo(theList.get(comparedIndex)) == 1)//greater th
+            else if(minElement.compareTo(theList.get(comparedIndex)) == 1)//gter th
             {
                 minElement = theList.get(comparedIndex);
                 swap(theList, comparedIndex, startIndex);
