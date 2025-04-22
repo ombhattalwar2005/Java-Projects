38c9a424f7a319995364c2d447ba56a8ceebea39
Om Bhattalwar <164125899+ombhattalwar2005@users.noreply.github.com>
2025-04-23 00:26:43 +0530

    Update Sorters2120.java

--- Sorters2120.java
+++ Sorters2120.java
@@ -80,7 +80,7 @@ public class Sorters2120 {
                     recursiveMergeSortHelper(theList, first, middle);
                 }
             }
-            else if(theList.get(first).compareTo(theList.get(last-1)) == 1)//if its out of order
+            else if(theList.get(first).compareTo(theList.get(last-1)) == 1)//if its out of orde
             {
                 swap(theList, first, middle);
                 if(first < middle)
