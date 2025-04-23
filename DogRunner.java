4e53e599d55220362208998fb9b90c01fe16b2b0
Om Bhattalwar <164125899+ombhattalwar2005@users.noreply.github.com>
2025-04-23 17:22:17 +0530

    Update DogRunner.java

--- DogRunner.java
+++ DogRunner.java
@@ -2,7 +2,6 @@ public class DogRunner{
 
     public static void main(String[] args) {
 
-
         Dog fifi = new Dog("Fifi", 6.3);
         System.out.println(fifi.getName());
         System.out.println(fifi.getWeight());
