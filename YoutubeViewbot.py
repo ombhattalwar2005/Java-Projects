2cb0e99422d88d041ada94f2928e240e9c8e16ee
Om Bhattalwar <164125899+ombhattalwar2005@users.noreply.github.com>
2025-04-24 00:40:25 +0530

    Update YoutubeViewbot.py

--- YoutubeViewbot.py
+++ YoutubeViewbot.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 from selenium import webdriver;
 
-#application
+#applicati
 def window():
     def runBot():
         #time to refresh page (seconds
