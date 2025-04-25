b24289a27c2f99b879b9f0e326e8fdff20f8d480
Om205 <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-04-25 18:57:37 +0530

    Update YoutubeViewbot.py

--- YoutubeViewbot.py
+++ YoutubeViewbot.py
@@ -8,7 +8,7 @@ from selenium import webdriver;
 
 def window():
     def runBot():
-        #time to refresh page 
+        #time to refresh pag
         Timer = int(timeInput.text())
 
         #youtube 
