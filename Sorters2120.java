03ea0ee6ef60c3154d1f7c2ca788cb6b5cc3fef3
Om205 <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-04-28 01:09:55 +0530

    Update Sorters2120.java

--- Sorters2120.java
+++ Sorters2120.java
@@ -13,6 +13,7 @@ public class Sorters2120 {
             
         }
     }
+    
 
     private static <T extends Comparable<T> > void swap(List<T> theList, int i1, int i2) {
         T temp = theList.get(i1);
