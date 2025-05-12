57388e77433cefa74e2c84f0a119b6cbc42270cc
Om Bhattalwar <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-05-12 20:14:02 +0530

    Update Sorters2120.java

--- Sorters2120.java
+++ Sorters2120.java
@@ -13,8 +13,7 @@ public class Sorters2120 {
                     
                 }
             }
-        }
-        
+        }   
     }
     
     private static <T extends Comparable<T> > void swap(List<T> theList, int i1, int i2) {
